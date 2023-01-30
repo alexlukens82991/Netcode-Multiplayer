ff770092ecbb1b5dd9a6220ff7447820339c8268
alexlukens82991 <78564818+alexlukens82991@users.noreply.github.com>
2023-01-30 17:48:43 -0500

    Little by little

--- Assets/Scripts/LukensBuilder/BuilderManager.cs
+++ Assets/Scripts/LukensBuilder/BuilderManager.cs
@@ -8,16 +8,91 @@ public class BuilderManager : NetworkBehaviour
 {
     public bool BuildModeOn;
 
-    public GameObject m_CurrentActiveBuildGhost;
+    public GhostController m_CurrentActiveBuildGhost;
+    public int m_CurrentGhost;
+    public List<NetworkObject> m_Ghosts;
+    public List<Transform> m_GhostPrefabs;
+
+
     [SerializeField] private Camera m_PlayerCam;
 
     [Header("Cache")]
     [SerializeField] private Transform m_Wall_Plank;
 
-    private void Start()
+    public override void OnNetworkSpawn()
     {
-        if (IsOwner)
-            StartCoroutine(RaycastRoutine());
+        if (!IsOwner) return;
+
+        Initialize();
+
+        StartCoroutine(RaycastRoutine());
+    }
+
+    private void Initialize()
+    {
+        IntializeServerRpc(OwnerClientId);
+    }
+
+    [ServerRpc]
+    private void IntializeServerRpc(ulong id)
+    {
+        print("intialize server rpc");
+        List<ulong> spawned = new();
+        foreach (Transform item in m_GhostPrefabs)
+        {
+            Transform newObj = Instantiate(item);
+
+            NetworkObject newNetworkObj = newObj.GetComponent<NetworkObject>();
+            
+            newNetworkObj.Spawn();
+            spawned.Add(newNetworkObj.NetworkObjectId);
+        }
+
+        
+        SaveSpawnedRefsClientRpc(spawned.ToArray(), id);
+    }
+
+    [ClientRpc]
+    public void SaveSpawnedRefsClientRpc(ulong[] spawnedObjects, ulong id)
+    {
+        print("Spawned Objects:");
+
+
+        foreach (var item in NetworkManager.Singleton.SpawnManager.SpawnedObjects)
+        {
+            print(item.Key + " | " + item.Value);
+        }
+
+        foreach (ulong item in spawnedObjects)
+        {
+            print(item);
+
+            NetworkObject foundObj = NetworkManager.Singleton.SpawnManager.SpawnedObjects[item];
+            print("found: " + foundObj.name);
+        }
+        
+        //List<NetworkObject> foundObjs = new();
+        //print("Saving spawned refs");
+        //foreach (ulong item in spawnedObjects)
+        //{
+        //    NetworkObject foundObj = NetworkManager.Singleton.SpawnManager.SpawnedObjects[item];
+
+        //    if (foundObj)
+        //    {
+        //        print("FOUND OBJECT!");
+        //    }
+        //    else
+        //    {
+        //        print("DID NOT FIND OBJECT");
+        //    }
+
+
+        //    foundObjs.Add(foundObj);
+        //}
+
+        //NetworkClient foundClient = NetworkManager.Singleton.ConnectedClients[id];
+
+        //foundClient.PlayerObject.GetComponent<BuilderManager>().m_Ghosts = foundObjs;
     }
 
     private void Update()
@@ -28,17 +103,26 @@ public class BuilderManager : NetworkBehaviour
         {
             SpawnWall();
         }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            m_CurrentActiveBuildGhost.CmdToggleVisibilityServerRpc();
+        }
     }
 
     IEnumerator RaycastRoutine()
     {
+        yield return new WaitForSeconds(1);
         do
         {
+
             GameObject foundObject = LukensUtilities.RaycastFromMouse(100, m_PlayerCam);
 
             if (foundObject != null)
             {
-                m_CurrentActiveBuildGhost.SetActive(true);
+                // show ghost
+                //m_CurrentActiveBuildGhost.GetComponent<NetworkObject>().;
+
 
                 string[] ghostExpanded = m_CurrentActiveBuildGhost.name.Split('_');
                 string[] foundExpanded = foundObject.name.Split('_');
@@ -50,7 +134,7 @@ public class BuilderManager : NetworkBehaviour
             }
             else
             {
-                m_CurrentActiveBuildGhost.SetActive(false);
+                // hide ghost
             }
             yield return new WaitForFixedUpdate();
         } while (BuildModeOn);
@@ -73,6 +157,7 @@ public class BuilderManager : NetworkBehaviour
 
     }
 
+
     [ServerRpc]
     private void SpawnWallServerRpc(ulong id, PosAndRotData posAndRotData)
     {
@@ -84,6 +169,8 @@ public class BuilderManager : NetworkBehaviour
 
         newWall.SetPositionAndRotation(newPos, newRot);
     }
+
+    
 
     public struct PosAndRotData : INetworkSerializable
     {
